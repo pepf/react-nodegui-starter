be0db32b716287b261e51e8a1fec91338efc7d4e
pepf <pepijnfens@gmail.com>
2019-08-18 15:17:50 +0200

    Notes App WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,75 @@
+import React, { useState, useMemo } from "react";
+import { Renderer, View, Text, Window, LineEdit } from "@nodegui/react-nodegui";
 import {
-  Renderer,
-  View,
-  Text,
-  Window,
-  Image,
-  ScrollArea
-} from "@nodegui/react-nodegui";
-import path from "path";
-import React from "react";
-import { AspectRatioMode } from "@nodegui/nodegui";
-
-import imageUrl from "../assets/nodegui.jpg";
-const distImgUrl = path.resolve(__dirname, imageUrl);
-const minSize = { width: 500, height: 400 };
+  QPushButtonEvents,
+  QLineEditEvents,
+  NativeEvent,
+  QKeyEvent
+} from "@nodegui/nodegui";
 
 const App = () => {
+  const [newNote, setNewNote] = useState("");
+  const [notes, setNotes] = useState([
+    { id: 1, createdAt: new Date(), text: "This is a note" }
+  ]);
+  const lineEditHandler = useMemo(
+    () => ({
+      [QLineEditEvents.textChanged]: (text: string) => {
+        setNewNote(text);
+      },
+      [QLineEditEvents.KeyRelease]: (e: NativeEvent) => {
+        const keyEvt = new QKeyEvent(e);
+        const code = keyEvt.text().charCodeAt(0);
+        if (code === 13) return; //ENTER button
+      }
+    }),
+    [newNote]
+  );
+  const buttonHandler = useMemo(
+    () => ({
+      [QPushButtonEvents.clicked]: () => {
+        setNotes([
+          ...notes,
+          { id: Math.random(), createdAt: new Date(), text: newNote }
+        ]);
+        setNewNote("");
+      }
+    }),
+    [notes, newNote]
+  );
+
   return (
-    <Window minSize={minSize} styleSheet={styleSheet}>
-      <ScrollArea style={`height: '100%';width: '100%';`}>
-        <View id="container">
-          <Text style="font-size: 15px; font-weight: bold; margin-bottom: 20px;">
-            Hello World
-          </Text>
-          <Image
-            style={imageStyle}
-            src={distImgUrl}
-            aspectRatioMode={AspectRatioMode.KeepAspectRatio}
-          />
+    <Window minSize={fixedSize} maxSize={fixedSize} styleSheet={styleSheet}>
+      <View id="container">
+        <View id="note_list">
+          <Text id="heading">Notes</Text>
+          {notes.map(note => (
+            <View id="note" key={note.id}>
+              <Text id="content_small">{note.createdAt.toDateString()}</Text>
+              <Text id="content">{note.text}</Text>
+            </View>
+          ))}
         </View>
-      </ScrollArea>
+        <View id="create_notes">
+          <LineEdit
+            text={newNote}
+            on={lineEditHandler}
+            id="create_notes_input"
+            placeholderText="Remember me to.."
+          />
+          <Button id="create_notes_button" text="Add" on={buttonHandler}>
+            Add
+          </Button>
+        </View>
+      </View>
     </Window>
   );
 };
 
-const imageStyle = `
-  height: "700px";
-  width: "700px";
-  qproperty-alignment: 'AlignHCenter';
-`;
+const theme = {
+  bg: "#111111",
+  text: "white"
+};
 
 const styleSheet = `
   #container {
@@ -47,9 +79,60 @@ const styleSheet = `
     width: '900';
     height: '900';
     flex-direction: column;
-    align-items: 'center';
-    justify-content: 'center';
-    background-color: 'grey';
+    min-height: '100%';
+    width: '100%';
+    align-items: 'flex-start';
+    justify-content: 'space-between';
+    background-color: ${theme.bg};
+  }
+  #heading {
+    font-size: 32px;
+    margin-bottom: 16px;
+    font-weight: bold;
+    color: ${theme.text};
+  }
+  #content {
+    font-size: 16px;
+    color: ${theme.text};
+  }
+  #content_small {
+    font-size: 12px;
+    color: #cccccc;
+  }
+  #note_list {
+    flex: 1;
+    flex-grow: 1;
+    flex-direction: column;
+    align-items: 'flex-start';
+    justify-content: 'flex-start';
+    padding: '5px';
+    overflow: scroll;
+  }
+  #note {
+    margin-vertical: 12px;
+  }
+  #create_notes {
+    width: '100%';
+    max-height: '40px';
+    padding: 5px;
+    border-top: 1px solid white;
+    flex: 1;
+    flex-direction: row;
+  }
+  #create_notes_input {
+    color: ${theme.text};
+    background-color: ${theme.bg};   
+    flex: 1;
+  }
+  #create_notes_button {
+    color: ${theme.text};
+    background-color: ${theme.bg};
+  }
+
+  #result {
+    font-size: 12px;
+    flex: 1;
+    color: cyan;
   }
 `;
 
